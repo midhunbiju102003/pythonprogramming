9b3e049327bbe982435d2a9950a87f7b88a41cc9
Midhun Biju <166794539+midhunbiju102003@users.noreply.github.com>
2024-11-30 01:03:09 -0800

    Add files via upload

---
+++ lamda_functions.py
@@ -0,0 +1,11 @@
+num = int(input("Enter the number:"))
+list = [i for i in range(1, num+1)if(1, num+1)if num % i == 0]
+print("\nEnter the length and breadth of a rectangle:")
+l = int(input("length:"))
+b = int(input("breadth:"))
+c = lambda x, y: x*y
+print("Area of rectangle:", c(l, b))
+print("\nEnter slide of a square:")
+s = int(input("Side of square:"))
+c = lambda x: x*x
+print("Area of square:", c(s))
