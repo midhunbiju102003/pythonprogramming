d0113aaf39eeaa8ddee00ae5f85ec4df5b65235e
Midhun Biju <166794539+midhunbiju102003@users.noreply.github.com>
2024-11-30 01:06:49 -0800

    lamda_functions.py

--- lamda_functions.py
+++ lamda_functions.py
@@ -1,5 +1,6 @@
 num = int(input("Enter the number:"))
 list = [i for i in range(1, num+1)if(1, num+1)if num % i == 0]
+print("factors of number=",list)
 print("\nEnter the length and breadth of a rectangle:")
 l = int(input("length:"))
 b = int(input("breadth:"))
