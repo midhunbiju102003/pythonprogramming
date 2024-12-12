bb7f535afcdb1f9653362407b443ec8b31ce643d
Midhun Biju <166794539+midhunbiju102003@users.noreply.github.com>
2024-12-11 22:46:24 -0800

    linebyline_copyline_csv.py

--- linebyline_copyline_csv.py
+++ linebyline_copyline_csv.py
@@ -1,11 +1,14 @@
+import csv
 def read_file_to_list(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
     return lines
 
+
 file_name = "example.txt"
 lines_list = read_file_to_list(file_name)
 print(lines_list)
+
 
 def copy_odd_lines(source_file, destination_file):
     with open(source_file, 'r') as source, open(destination_file, 'w') as dest:
@@ -17,7 +20,7 @@ source_filename = "source1.txt"
 destination_filename = "odd_lines.txt"
 copy_odd_lines(source_filename, destination_filename)
 
-import csv
+
 def read_csv_rows(filename):
     rows = []
     with open(filename, 'r') as file:
@@ -26,6 +29,7 @@ def read_csv_rows(filename):
             rows.append(",".join(row))
     return rows
 
-csv_filename = "example.csv"
+
+csv_filename = "example1.csv"
 rows_list = read_csv_rows(csv_filename)
 print(rows_list)
